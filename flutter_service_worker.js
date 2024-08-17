f128990cda480c37eda116597054db68391c14a8
calebcng <calebcng@gmail.com>
2024-08-17 15:42:12 +0700

    Test stock textfield with resizToAvoidBottomInsets true

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "594af0db531538b45d60b082b1019127",
-"index.html": "eb3d102dc88fd1bcba368ebcd3e3c521",
-"/": "eb3d102dc88fd1bcba368ebcd3e3c521",
-"main.dart.js": "487ef3f7e52d2c58cbed3193c8876157",
+"index.html": "b8f535b4dc7f731a37bd3011579f3243",
+"/": "b8f535b4dc7f731a37bd3011579f3243",
+"main.dart.js": "af6982ab1bf40f40ef8f3af34094581f",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
